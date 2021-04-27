12c7dd010b74e297e9a81432125477fe0f70ad47
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-27 10:51:19 +0530

    Add show_index view in quora app
    Add this view to get home page

--- quoraapp/views.py
+++ quoraapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Question, Answer, Comment
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def show_index(request):
+    context = {
+        'No_Of_Question': Question.objects.all().count(),
+        'No_Of_Answer': Answer.objects.all().count(),
+        'No_Of_Comment': Comment.objects.all().count(),
+        'No_Of_User': User.objects.all().count(),
+    }
+    return render(request, 'quoraapp/index.html', context)
