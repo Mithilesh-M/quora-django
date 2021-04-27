72f442b30eba268a13d5fd153df3c0b7add964fd
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-27 15:34:17 +0530

    Update comment answer create view in quora app
    Update to add user automatically

--- quoraapp/views.py
+++ quoraapp/views.py
@@ -120,12 +120,14 @@ class CommentUpdateViewQuestion(LoginRequiredMixin, generic.UpdateView):
 
 class CommentCreateViewAnswer(LoginRequiredMixin, generic.CreateView):
     model = Comment
-    fields = ['comment','answer', 'question', 'vote', 'user']
+    fields = ['comment','answer', 'question', 'vote']
 
-    def get_success_url(self):
-        comment_id = self.object.id
-        question = Comment.objects.get(pk=comment_id).answer.question
-        return reverse_lazy('question-detail', kwargs={'pk': question.id})
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.user = User.objects.get(pk=self.request.user.id)
+        comment.save()
+        self.success_url = reverse('question-detail', kwargs={'pk': comment.answer.question.id})
+        return super(CommentCreateViewAnswer, self).form_valid(form)
 
 
 class CommentDeleteViewAnswer(LoginRequiredMixin, generic.DeleteView):
